3acaca68333484f12e4d375a4a24a069dbac3427
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-03-21 16:19:12 +0900

    Revert "sobogil scorepage"

--- SE_Tetris/src/main/java/module-info.java
+++ SE_Tetris/src/main/java/module-info.java
@@ -7,7 +7,6 @@ module com.example.fxtest {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
-    requires java.sql;
 
     opens com.example.fxtest to javafx.fxml;
     exports com.example.fxtest;
